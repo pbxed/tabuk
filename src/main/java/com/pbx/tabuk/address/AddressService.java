cf9f1a89f067c5f52ea91bf5d0a83e539cb8e335
pbxed <jshenolikar@gmail.com>
2022-01-26 09:11:10 +0000

    add unit test to address controller

--- src/main/java/com/pbx/tabuk/address/AddressService.java
+++ src/main/java/com/pbx/tabuk/address/AddressService.java
@@ -2,11 +2,19 @@ package com.pbx.tabuk.address;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class AddressService {
+
+    private final AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
     public List<Address> getAddresses() {
-        return null;
+        return addressRepository.selectAddresses();
     }
 }
